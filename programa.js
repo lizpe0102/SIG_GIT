bd50a57ae52be2b4c8c2e1cbec35281fd3758290
lizethpeña <lizpe0102@gmail.com>
2025-02-05 20:31:22 -0500

    v 1.1

--- programa.js
+++ programa.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.742777, -74.108429], 18);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
