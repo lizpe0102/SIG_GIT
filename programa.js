3faffed7b3519724c143cb5e619136df1b13a1fc
lizethpeña <lizpe0102@gmail.com>
2025-02-05 20:47:37 -0500

    1.2

--- programa.js
+++ programa.js
@@ -3,3 +3,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([4.742430842156989, -74.10784158543846]).addTo(map)
+.bindPopup('<b>Punto de Interés</b><br>Monumento La Gaitana.')
+.openPopup();
+var marker = L.marker([4.742246403321685, -74.10817686154299]).addTo(map)
+.bindPopup('<b>Punto de Interés</b><br>Iglesia La Gaitana.')
+.openPopup();
